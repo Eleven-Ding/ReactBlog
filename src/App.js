3dc89f242c283fc6867a19837bf2339315e0cdf8
mylove-zqy <1559298665@qq.com>
2021-12-22 11:30:51 +0800

    feat: 初始化前端埋点代码

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import React from "react";
 import store from "./store";
 //组件
 import DSYMain from "./pages/main/index";
-
 // import { Provider, KeepAlive } from "react-keep-alive";
 import { Provider } from "react-redux";
+
 function App() {
   return (
     <Provider store={store}>
---
+++ src/utils/diylog.js
@@ -0,0 +1,22 @@
+console.log(
+    "%c跟我走吧",
+    "color:#ff5777;text-shadow: 0 1px 0 #ff5777,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em"
+);
+console.log(
+    "%c我写代码养你",
+    " text-shadow: 0 1px 0 #ccc,0 2px 0 #c9c9c9,0 3px 0 #bbb,0 4px 0 #b9b9b9,0 5px 0 #aaa,0 6px 1px rgba(0,0,0,.1),0 0 5px rgba(0,0,0,.1),0 1px 3px rgba(0,0,0,.3),0 3px 5px rgba(0,0,0,.2),0 5px 10px rgba(0,0,0,.25),0 10px 10px rgba(0,0,0,.2),0 20px 20px rgba(0,0,0,.15);font-size:5em"
+);
+console.log(
+    "%c ",
+    "color:red;font-size:30px;font-weight:bolder;padding:50px 420px;line-height:10px;background:url('http://img.zcool.cn/community/0127c0577e00620000012e7e12da0e.gif') no-repeat;background-size:contain;"
+);
+console.log(
+    "%c你喜欢西瓜汁,草莓汁,芒果汁,还是我这个小比崽汁？",
+    "color: #fff; background: #f40; font-size: 20px;border-radius:0 15px 15px 0;padding:10px;"
+);
+console.error("嘿！我们是不是哪里见过!"); // 打印错误
+console.error("-------------------"); // 打印错误
+
+console.error("有些事情真的很难去实现的"); // 打印错误
+console.error("不过没关系吖！世界还有好多事情等着你去探索呢！"); // 打印错误
+console.error("加油！陌生人!"); // 打印错误
